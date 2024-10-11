ff383188ece03beee3e609089298645adbeec89a
Anja <anja.djakovic@student.etf.unibl.org>
2024-10-11 18:41:08 +0200

    Issue #10: Write main user space application

--- src/main.c
+++ src/main.c
@@ -1,3 +1,29 @@
+/****************************************************************************
+ * Copyright (C) 2024 by Anja Djakovic                                      *
+ *                                                                          *
+ * This file is part of Home-Security-System                                *
+ *                                                                          *
+ *   Home-Security-System is free solution: you can redistribute it and/or  *
+ *   modify it.                                                             *
+ *   Home-Security-System is distributed in the hope that it will be useful *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                   *
+ *                                                                          *
+ *   See the README.md and WikiPage for more details.                       *
+ *                                                                          *
+ ****************************************************************************/
+/**
+ * @file main.c
+ * @author Anja Djakovic
+ * @date 11 Oct 2024
+ * @brief File containing main user-space program for Home-Security-System
+ *
+ * This .c file is user-space program  which represents the bridge between
+ * user and hardware devices such as ADC, BUZZER, LEDS, TIMER. It consists
+ * of main thread, thread for reading ADC data, thread for tracking timer. 
+ * @see https://github.com/AnjaDj/Home-Security-System/wiki
+ */
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,52 +45,46 @@ const char* message_for_BUZZER      = "10";
 const char* message_for_TIMER_start = "start";
 const char* message_for_TIMER_stop  = "stop";
 
- /* Mutex controlling servo movement critical section */
-static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+volatile uint8_t door_opened = 0x00;   /** Flag to indicate when sensor has detected an object  - door is opened  */
+volatile uint8_t cancel_timer= 0x00;   /** Flag to indicate user has stopped the timer - correct password entered */
+volatile uint8_t time_is_up  = 0x00;   /** Flag to indicate timer has counted 30secs and correct password wasnt entered */
 
-/* Flag to indicate when sensor has detected an object, used to restart the semaphore */
-volatile uint8_t door_opened = 0x00;
-volatile uint8_t cancel_timer= 0x00;
-volatile uint8_t time_is_up  = 0x00;
-
-/* Paths to char driver files */
+/** Paths to char driver files */
 const char* LED_DRIVER   = "/dev/LED_driver";
 const char* BUZZ_DRIVER  = "/dev/BUZZ_driver";
 const char* ADC_DRIVER   = "/dev/ADC_driver";
 const char* TIMER_DRIVER = "/dev/TIMER_driver";
 
-/* File descriptors for all driver files after opening */
+/** File descriptors for all driver files after opening */
 int led_fd, buzz_fd, adc_fd, timer_fd;
 
-/* Function that opens all device files and checks for errors */
+/** Function that opens all device files and checks for errors */
 int open_drivers(void);
 
-/* SIGINT handler function, closes driver files */
+/** SIGINT handler function, closes driver files */
 void kill_handler(int signo, siginfo_t *info, void *context);
 
-/*  Thread function reading data from ADC (sensor), comparing it to threshold value */
+/**  Thread function reading data from ADC (sensor), comparing it to threshold value */
 void* sensor_run(void* arg)
 {
     char data[4];
-	
-    const uint32_t THRESHOLD =0x00000401;
+    const uint32_t THRESHOLD = 0x00000401;
 	
     while(1)
     {
         read(adc_fd, data, 4);
-	
-	usleep(50000);
-	
-	//printf("Reading data = %x %x %x %x\n", data[3],data[2],data[1],data[0]);
+	    usleep(50000);
+	    //printf("Reading data = %x %x %x %x\n", data[3],data[2],data[1],data[0]);
 		
-	uint32_t result_data = data[3]<<24 | data[2]<<16 | data[1]<<8 | data[0] ; // Kombinovanje 4 bajta u 32-bitni rezultat
+	    uint32_t result_data = data[3]<<24 | data[2]<<16 | data[1]<<8 | data[0] ;
 	
-	printf("Data from distance sensor = %x\n",result_data);
+	    printf("Data from distance sensor = %x\n",result_data);
 
-        if(result_data > THRESHOLD)		// Detektovali smo otvaranje ulaznih vrata
-	{
+        /** door is opened */
+        if(result_data > THRESHOLD)
+	    {
             door_opened = 0xff;
-	    pthread_exit(NULL);
+	        pthread_exit(NULL);
         }
     }
 }
@@ -72,30 +92,28 @@ void* sensor_run(void* arg)
 static char status[1];
 ssize_t ret_val;
 
-/*  Thread function for timer*/
+/**  Thread function for timer*/
 void* timer_run(void* arg)
 {
     write(timer_fd, message_for_TIMER_start, strlen("start"));
     usleep(50000);
     
-    while(time_is_up != 0xff && cancel_timer != 0xff) //sve dok vrijeme nije isteklo ili sve dok timer nije cancelovan
+    while(time_is_up != 0xff && cancel_timer != 0xff)
     {
-	ret_val = read(timer_fd, status, 1);
+	    ret_val = read(timer_fd, status, 1);
 	
-	if(status[0]==0x00000000) time_is_up = 0xff;    /* 30secs have expired. Timer has counted down to 0 */
-	if(status[0]==0x00000010) cancel_timer = 0xff;  /* Timer is stopped by user. Timer hasnt exceeded 0 */
+	    if(status[0]==0x00000000) time_is_up   = 0xff;    /** 30secs have expired. Timer has counted down to 0 */
+	    if(status[0]==0x00000010) cancel_timer = 0xff;    /** Timer is stopped by user. Timer hasnt exceeded 0 */
 	
-	usleep(50000);
+	    usleep(50000);
     }
     pthread_exit(NULL);
 }
 
-/* Main thread */
+/** Main thread */
 int main(void)
 {
-    /* Nit za ocitavanje sa senzora */
     pthread_t sensor_thread;
-    /* Nit za tajmer*/
     pthread_t timer_thread;
 	
     char sifra[BUF_LEN]="";
@@ -113,51 +131,62 @@ int main(void)
         return -1;
     }
 
-    /* Kreiramo i pokrecemo nit koja je zaduzena za ocitavanje vrijednosti sa senzora */
-    pthread_create(&sensor_thread, NULL, sensor_run, NULL);
+    pthread_create(&sensor_thread, NULL, sensor_run, NULL);     /** Creates and runs the sensor thread */
 
     while(1){
-        /* Neko je usao u prostoriju, pokrece se sigurnosna procedura */
-	if (door_opened == 0xff){
+    
+        /* Someone has entered the room, activate security procedure */
+	    if (door_opened == 0xff){
 			
 	    write(led_fd, message_for_YELLOW_LED, BUF_LEN);
-			
-	    /* Kreiramo i pokrecemo tajmersku nit */
-	    pthread_create(&timer_thread, NULL, timer_run, NULL);
+	    pthread_create(&timer_thread, NULL, timer_run, NULL);  /** Creates and runs the timer thread */
 	
-	    /* dok vrijeme nije isteklo */
 	    while(time_is_up != 0xff)
 	    {
-				/* Unos sifre */
+				/* Enter the password */
 				printf("Unesite sifru: ");
 				scanf("%s",sifra);
 				
-				if( (strcmp(sifra,PASSWORD) == 0) && (time_is_up != 0xff) ){ // Ako je sifra ispravna i vrijeme nije isteklo
+				if( (strcmp(sifra,PASSWORD) == 0) && (time_is_up != 0xff) ){        /** correct password on time */
 					write(led_fd, message_for_GREEN_LED, BUF_LEN);
 					cancel_timer = 0xff;
 					break;
 				}
-				else if ((strcmp(sifra,PASSWORD) != 0) && (time_is_up != 0xff)){	// Unesena neispravna sifra i vrijeme nije isteklo
+				else if ((strcmp(sifra,PASSWORD) != 0) && (time_is_up != 0xff)){    /** wrong password on time */
 					write(buzz_fd, message_for_BUZZER, BUF_LEN);
 					write(led_fd, message_for_RED_LED, BUF_LEN);
 				}
 	    }
+	    printf("time_is_up   = %x\n",time_is_up);
+	    printf("cancel_timer = %x\n",cancel_timer);
+	    
 	    if (cancel_timer == 0xff){ 
-		write(timer_fd, message_for_TIMER_stop, strlen("stop"));
-		break;
+		    write(timer_fd, message_for_TIMER_stop, strlen("stop"));
+		    break;
 	    }
 	    
 	    if (time_is_up == 0xff)
 	    {
 	        write(buzz_fd, message_for_BUZZER, strlen(message_for_BUZZER));
-		write(led_fd, message_for_RED_LED, BUF_LEN);
-		break;
+		    write(led_fd, message_for_RED_LED, BUF_LEN);
+		
+		    /** snapshoot - run camera */
+		    int ret = system("./camera_run.sh");
+		
+		    if (ret < 0){
+		        printf("Greska prilikom pozivanja skripte camera_run.sh\n");
+		    }else{
+		        printf("Skripta camera_run.sh pozvana\n");
+		    }
+		
+		    break;
 	    }
 	}
 		
     }
 	pthread_join(sensor_thread, NULL);
 	pthread_join(timer_thread, NULL);
+	printf("main exit\n");
     return 0;
 }
 
@@ -185,7 +214,7 @@ void kill_handler(int signo, siginfo_t *info, void *context)
         close(led_fd);
         close(buzz_fd);
         close(adc_fd);
-	close(timer_fd);
+	    close(timer_fd);
         exit(1);
     }
 }
